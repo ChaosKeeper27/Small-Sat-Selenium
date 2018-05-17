a479c72c9d44d45ee9e2c8c53c14002ae0288664
ChaosKeeper27 <choggan0124@gmail.com>
2018-05-17 12:14:53 -0600

    big update

--- SatMining/SScode.py
+++ SatMining/SScode.py
@@ -11,6 +11,7 @@ import xlwt # write
 import string
 import re
 
+# dayCounter = 0
 
 class AuthorStruct():
     def __init__(self):
@@ -26,8 +27,29 @@ class ExcelEntry():
         self.multimedia = "youtube"
         self.researchArea = "" # needs to be grabbed per session for a from a list of keyed terms
         self.session = "" # can be copied from title split
-        self.startDate = "" # needs to be in a very specific format, if python can't date format it OpenRefine will
+        self.startDate = "" # needs to be in a very specific format, if python can't date-format it OpenRefine will
 
+def CreateDate(rawTime):
+    year = "2018"
+    month = "08"
+    day = "01" # going to need some logic to determine the day here
+    hour = ""
+    minutes = ""
+    seconds = "00"
+    timeData = rawTime.split()
+    AMPM = timeData[1]
+    timeBreakdown = timeData[0].split(":")
+    minutes = timeBreakdown[1]
+    if AMPM == "PM":
+        hour = str(int(timeBreakdown[0]) + 12) # convert to military time
+    else: # AM time
+        if len(timeBreakdown[0] == 1):
+            hour = "0" + timeBreakdown[0] # add 0 infront of single digits
+        else:
+            hour = timeBreakdown[0]
+    correctFormat = year + "-" + month + "-" + day + "T" + hour + ":" + minutes + ":" + seconds + "Z"
+    print correctFormat
+    return correctFormat
 
 
 def main():
@@ -35,9 +57,8 @@ def main():
     # sheet = book.add_sheet('Sheet 1')
     # book.save("Sample.xls")
 
-    # driver = webdriver.Chrome("C:\\Users\\Casey\\Downloads\\chromedriver_win32\\chromedriver.exe") # works on Casey's Laptop (0riginal)
-    driver = webdriver.Chrome("chromedriver.exe") # File path for Casey
-    # driver = webdriver.Chrome() # File path for Jesse
+    driver = webdriver.Chrome("chromedriver.exe")
+    Entry_Data = ExcelEntry()
 
     driver.get("https://smallsat.org")
     linksArray = ["https://www.smallsat.org/technical-program/tech-sessions", "https://www.smallsat.org/technical-program/workshop", "https://www.smallsat.org/technical-program/keynote"]
@@ -46,25 +67,39 @@ def main():
     print "# of Sessions: " + str(sessionsList.__len__())
 
     i = 0
-    alternateKey = 0
+
     # while i < len(sessionsList): # cycle through sessions
     while i < 1:  # single test
-        print sessionsList[i].text
+        alternateKey = 0 # reset each session
+        sessionTitleText = sessionsList[i].text
+        sessionData = sessionTitleText.split() # break up session title
+        # ---- Logic of determining "day" here *I'm working on it
+
+        if sessionData[0] == "Session": # roman numeral conversion
+            romanNum = ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX', 'X', 'XI', 'XII', 'XIII']
+            convertNum = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+            for numCheck in xrange(12):
+                if sessionData[1][:-1] == romanNum[numCheck]:
+                    sessionData[1] = str(convertNum[numCheck]) + ":"
+            dataString = ""
+            for addData in xrange(sessionData.__len__()):
+                dataString += sessionData[addData] + " "
+            Entry_Data.session = dataString
+        else:
+            Entry_Data.session = sessionTitleText # poster and swifty names caught here
+        print Entry_Data.session
         sessionsList[i].click() # opens drop down list per session
-        # ---- We will need to use session text to code each entry for DC ----
+
         time.sleep(5)
 
         eventsList = sessionsList[i].find_elements_by_css_selector("p")
         print "# of Events: " + str(eventsList.__len__())
 
-        j = 4 # ignore first two elements, should be 2
-        while j in xrange(len(eventsList) + 2): # cycle through events
-        # while j < 3:  # single test
+        j = 2 # ignore first two elements, should be 2
+        while j in xrange(len(eventsList) - 1): # cycle through events
 
-            # print eventsList[j].text
             eventData = eventsList[j].text # data on separate lines
             splitData = eventData.splitlines() # lines divided into list elements
-            # time.sleep(10)
 
             if splitData[0] == "Alternates:":
                 alternateKey = 1
@@ -72,28 +107,24 @@ def main():
             if alternateKey == 0: # not an alternate/has time stamp
                 print splitData[0]  # Time
                 # --- Time needs to be formatted properly!!! ---
+                formattedDate = CreateDate(splitData[0])
+                Entry_Data.startDate = formattedDate
                 print splitData[1]  # Event Title
-
-                # print splitData[2]  # Authors/Presenters
+                Entry_Data.title = splitData[1]
 
                 authorsAff = splitData[2].split("; ") # puts authors with their affiliation
 
                 for k in xrange(len(authorsAff)): # cycle through each affiliated group
                     print "# of Aff: " + str(len(authorsAff))
                     print authorsAff[k]
-                    # authorTest = authorsAff[k].encode('ascii', 'replace').replace(u'\ufffd', '-')
-                    # authorTest = authorsAff[k].replace(u'\ufffd', "-", 1)
-                    # print "Ascii test: " + authorTest
-                    # authorTest = authorsAff[k].decode('ascii', 'replace').replace(u'\ufffd', '-')
-                    # authorGroup = authorTest.split("- ") # puts authors by themselves
-                    # authorGroup = authorsAff[k].split("- ")  # puts authors by themselves
-                    authorGroup = re.split('-|,-|', authorsAff[k])
+
+                    authorGroup = re.split(u'- |- |– ', authorsAff[k])
 
                     print authorGroup[0]
                     print authorGroup[1]
 
                     authorSingle = authorGroup[0].split(", ") # divides authors up
-                    # print authorSingle[0]
+
                     for m in xrange(len(authorSingle)): # cycle through each author
 
                         splitName = authorSingle[m].split() # splits authors name apart
@@ -123,11 +154,56 @@ def main():
                             print splitName[2]
                         # ---- Entry should be written out to spreadsheet --- #
 
-
             else: # alternateKey = 1
                 # i must be referenced as i + 1, "Alternates:" is considered an element and should not be processed
-                print splitData[0]  # Event Title
-                print splitData[1]  # Authors/Presenters
+                if splitData[0] == "Alternates:":
+                    print "---Alternates Here---"
+                else: # do the stuff
+                    print splitData[0]  # Event Title
+                    Entry_Data.title = splitData[0]
+                    print splitData[1]  # Authors/Presenters
+
+                    authorsAff = splitData[1].split("; ")  # puts authors with their affiliation
+
+                    for k in xrange(len(authorsAff)):  # cycle through each affiliated group
+                        print "# of Aff: " + str(len(authorsAff))
+                        print authorsAff[k]
+
+                        authorGroup = re.split(u'- |- |– ', authorsAff[k])
+
+                        print authorGroup[0]
+                        print authorGroup[1]
+
+                        authorSingle = authorGroup[0].split(", ")  # divides authors up
+
+                        for m in xrange(len(authorSingle)):  # cycle through each author
+
+                            splitName = authorSingle[m].split()  # splits authors name apart
+
+                            authorMeta = AuthorStruct()  # needs to be regenerated each time
+                            authorMeta.aff = authorGroup[1]  # grab affiliation
+                            if len(splitName) == 0:
+                                print "ERROR - No ENTRY"
+                            elif len(splitName) == 2:
+                                authorMeta.fname = splitName[0]
+                                authorMeta.lname = splitName[1]
+                                print splitName[0]
+                                print splitName[1]
+                            elif len(splitName) == 3:
+                                authorMeta.fname = splitName[0]
+                                authorMeta.mname = splitName[1]
+                                authorMeta.lname = splitName[2]
+                                print splitName[0]
+                                print splitName[1]
+                                print splitName[2]
+                            else:  # len(splitName) == 4:
+                                authorMeta.fname = splitName[0]
+                                authorMeta.mname = splitName[1]
+                                authorMeta.lname = splitName[2] + splitName[3]
+                                print splitName[0]
+                                print splitName[1]
+                                print splitName[2]
+                            # ---- Entry should be written out to spreadsheet --- #
 
             j += 1
         i += 1
