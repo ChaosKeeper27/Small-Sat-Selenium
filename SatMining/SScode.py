548be53fe968794be34053d6fbf1ff4a9e0b7f0f
ChaosKeeper27 <choggan0124@gmail.com>
2018-06-04 12:27:12 -0600

    superUpdate

--- SatMining/SScode.py
+++ SatMining/SScode.py
@@ -11,24 +11,41 @@ import xlwt # write
 import string
 import re
 
-#
-# dayCounter = 0
+class AuthorStruct(object):
+    fname = "NULL"
+    mname = "NULL"
+    lname = "NULL"
+    suffix = "NULL"
+    email = "NULL"
+    aff = "NULL"
+    corporate = "FALSE"
 
-class AuthorStruct():
-    def __init__(self):
-        self.fname = ""
-        self.mname = ""
-        self.lname = ""
-        self.aff = ""
-        self.corporate = "FALSE"
-
-class ExcelEntry():
-    def __int__(self):
-        self.title = ""
-        self.multimedia = "youtube"
-        self.researchArea = "" # needs to be grabbed per session for a from a list of keyed terms
-        self.session = "" # can be copied from title split
-        self.startDate = "" # needs to be in a very specific format, if python can't date-format it OpenRefine will
+class ExcelEntry(object):
+    complete_Entry =[]
+    totalAuthorMeta = []
+    authorCounter = 0
+    excelRowCount = 1
+    title = "NULL"
+    fulltext = "NULL"
+    keywords = "NULL"
+    abstract = "NULL"
+    disciplines = "NULL"
+    comments = "NULL"
+    dcmi = "NULL"
+    embargo = "NULL"
+    endDate = "NULL"
+    funder = "NULL"
+    grant = "NULL"
+    hosted = "NULL"
+    location = "NULL"
+    multimediaURL = "NULL"
+    multimedia = "youtube"
+    previousVersions = "NULL"
+    researchArea = "NULL" # needs to be grabbed per session for a from a list of keyed terms
+    session = "NULL" # can be copied from title split
+    startDate = "NULL" # needs to be in a very specific format, if python can't date-format it OpenRefine will
+    updateReason = "NULL"
+    url = "NULL"
 
 def CreateDate(rawTime):
     year = "2018"
@@ -52,23 +69,85 @@ def CreateDate(rawTime):
     print correctFormat
     return correctFormat
 
+def checkItem(item2Add):
+    if item2Add == "NULL":
+        return ""
+    else:
+        return item2Add
 
-def convertToRomanNum(sessionData, Entry_Data, sessionTitleText):
+def generateRowEntry(Entry_Data):
+    organizedRow = []
+    Entry_Data.complete_Entry.append(checkItem(Entry_Data.title))
+    Entry_Data.complete_Entry.append(checkItem(Entry_Data.fulltext))
+    Entry_Data.complete_Entry.append(checkItem(Entry_Data.keywords))
+    Entry_Data.complete_Entry.append(checkItem(Entry_Data.abstract))
+    Entry_Data.complete_Entry.append(checkItem(Entry_Data.disciplines))
+    Entry_Data.complete_Entry.append(checkItem(Entry_Data.comments))
+    Entry_Data.complete_Entry.append(checkItem(Entry_Data.dcmi))
+    Entry_Data.complete_Entry.append(checkItem(Entry_Data.embargo))
+    Entry_Data.complete_Entry.append(checkItem(Entry_Data.endDate))
+    Entry_Data.complete_Entry.append(checkItem(Entry_Data.funder))
+    Entry_Data.complete_Entry.append(checkItem(Entry_Data.grant))
+    Entry_Data.complete_Entry.append(checkItem(Entry_Data.hosted))
+    Entry_Data.complete_Entry.append(checkItem(Entry_Data.location))
+    Entry_Data.complete_Entry.append(checkItem(Entry_Data.multimediaURL))
+    Entry_Data.complete_Entry.append(checkItem(Entry_Data.multimedia))
+    Entry_Data.complete_Entry.append(checkItem(Entry_Data.previousVersions))
+    Entry_Data.complete_Entry.append(checkItem(Entry_Data.researchArea))
+    Entry_Data.complete_Entry.append(checkItem(Entry_Data.session))
+    Entry_Data.complete_Entry.append(checkItem(Entry_Data.startDate))
+    Entry_Data.complete_Entry.append(checkItem(Entry_Data.updateReason))
+    Entry_Data.complete_Entry.append(checkItem(Entry_Data.url))
+    Entry_Data.complete_Entry = Entry_Data.complete_Entry + Entry_Data.totalAuthorMeta
+    if Entry_Data.authorCounter < (len(Entry_Data.totalAuthorMeta)/ 7):
+        Entry_Data.authorCounter = (len(Entry_Data.totalAuthorMeta)/ 7)
+    return
+
+def convertToRomanNum(sessionData, Entry_Data, sessionTitleText, driver):
     if sessionData[0] == "Session":  # roman numeral conversion
         romanNum = ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX', 'X', 'XI', 'XII', 'XIII']
         convertNum = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
         for numCheck in xrange(12):
-            if sessionData[1][:-1] == romanNum[numCheck]:               #check individual words for Roman Numeral
-                sessionData[1] = str(convertNum[numCheck]) + ":"        #Replace Roman Numeral with number
+            if sessionData[1][:-1] == romanNum[numCheck]:               # check individual words for Roman Numeral
+                sessionData[1] = str(convertNum[numCheck]) + ":"        # Replace Roman Numeral with number
+                twoDigit = ""
+                if numCheck + 1 < 10: twoDigit = "0" + str(numCheck + 1)
+                else: twoDigit = str(numCheck + 1)
+                pageTitle = driver.find_element_by_css_selector("div[id^='page-title']")
+                if str(pageTitle.text) == "TECHNICAL SESSIONS":
+                    print "TPS" + twoDigit + "-2018"
+                    Entry_Data.researchArea = "TPS" + twoDigit + "-2018"
+                else: # pageTitle == "Pre-Conference"
+                    print "PWS" + twoDigit + "-2018"
+                    Entry_Data.researchArea = "PWS" + twoDigit + "-2018"
+
         dataString = ""
         for addData in xrange(sessionData.__len__()):
             dataString += sessionData[addData] + " "
         Entry_Data.session = dataString
     else:
         Entry_Data.session = sessionTitleText  # poster and swifty names caught here
+        areaSplit = sessionTitleText.split()
+        if areaSplit[0] == "Poster":
+            Entry_Data.researchArea = "Poster" + areaSplit[2] + "-2018"
+        else: # areaSplit[0] == "Swifty"
+            Entry_Data.researchArea = "Swifty" + areaSplit[2] + "-2018"
     return
 
-def splitAuthorName (authorSingle, authorGroup):              # splits author's name into first and last
+def addToTotalAuthorMeta(eachAuthorData, Entry_Data):
+    for g in xrange(len(eachAuthorData)):
+        Entry_Data.totalAuthorMeta.append(checkItem(eachAuthorData[g].fname))
+        Entry_Data.totalAuthorMeta.append(checkItem(eachAuthorData[g].mname))
+        Entry_Data.totalAuthorMeta.append(checkItem(eachAuthorData[g].lname))
+        Entry_Data.totalAuthorMeta.append(checkItem(eachAuthorData[g].suffix))
+        Entry_Data.totalAuthorMeta.append(checkItem(eachAuthorData[g].email))
+        Entry_Data.totalAuthorMeta.append(checkItem(eachAuthorData[g].aff))
+        Entry_Data.totalAuthorMeta.append(checkItem(eachAuthorData[g].corporate))
+
+def splitAuthorName (authorSingle, authorGroup, Entry_Data): # splits author's name into first and last
+
+    eachAuthorData = []
+
     for m in xrange(len(authorSingle)):                       # cycle through each author
 
         splitName = authorSingle[m].split()                   # splits authors name apart
@@ -80,48 +159,90 @@ def splitAuthorName (authorSingle, authorGroup):              # splits author's 
         elif len(splitName) == 2:
             authorMeta.fname = splitName[0]
             authorMeta.lname = splitName[1]
-            print splitName[0]
-            print splitName[1]
         elif len(splitName) == 3:
             authorMeta.fname = splitName[0]
             authorMeta.mname = splitName[1]
             authorMeta.lname = splitName[2]
-            print splitName[0]
-            print splitName[1]
-            print splitName[2]
         else:  # len(splitName) == 4:
             authorMeta.fname = splitName[0]
             authorMeta.mname = splitName[1]
             authorMeta.lname = splitName[2] + splitName[3]
-            print splitName[0]
-            print splitName[1]
-            print splitName[2]
+        eachAuthorData.append(authorMeta) # logs author objects for processing
+    addToTotalAuthorMeta(eachAuthorData, Entry_Data)
+
+
     return
 
-def separateAuthors (authorsAff):                               #separates authors from affiliations and divides into
-                                                                #individual authors
+def write_to_File(sheet, Entry_Data):
+    for x in xrange(len(Entry_Data.complete_Entry)):
+        sheet.write(Entry_Data.excelRowCount, x, Entry_Data.complete_Entry[x])
+    Entry_Data.excelRowCount += 1
+    return
+
+def separateAuthors (authorsAff, Entry_Data):                               # separates authors from affiliations and divides into
+                                                                # individual authors
     for k in xrange(len(authorsAff)):                           # cycle through each affiliated group
         print "# of Aff: " + str(len(authorsAff))
         print authorsAff[k]
 
         authorGroup = re.split(u'- |- |– ', authorsAff[k])
-
         print authorGroup[0]
         print authorGroup[1]
 
         authorSingle = authorGroup[0].split(", ")               # divides authors up
 
-        splitAuthorName(authorSingle, authorGroup)
+        splitAuthorName(authorSingle, authorGroup, Entry_Data)
+
     return
 
+def createInitialHeaders(sheet):
+    sheet.write(0, 0, "title")
+    sheet.write(0, 1, "fulltext_url")
+    sheet.write(0, 2, "keywords")
+    sheet.write(0, 3, "abstract")
+    sheet.write(0, 4, "disciplines")
+    sheet.write(0, 5, "comments")
+    sheet.write(0, 6, "dcmi_type")
+    sheet.write(0, 7, "embargo_date")
+    sheet.write(0, 8, "end_date")
+    sheet.write(0, 9, "funder")
+    sheet.write(0, 10, "grant")
+    sheet.write(0, 11, "hosted")
+    sheet.write(0, 12, "location")
+    sheet.write(0, 13, "multimedia_url")
+    sheet.write(0, 14, "multimedia_format")
+    sheet.write(0, 15, "previous_versions")
+    sheet.write(0, 16, "research_area")
+    sheet.write(0, 17, "session")
+    sheet.write(0, 18, "start_date")
+    sheet.write(0, 19, "update_reason")
+    sheet.write(0, 20, "url")
+    return
+
+def createAuthorHeaders(sheet, authorMaxCount):
+    o = 21
+    for m in xrange(authorMaxCount):
+        sheet.write(0, o, "author" + str(m + 1) + "_fname")
+        sheet.write(0, o + 1, "author" + str(m + 1) + "_mname")
+        sheet.write(0, o + 2, "author" + str(m + 1) + "_lname")
+        sheet.write(0, o + 3, "author" + str(m + 1) + "_suffix")
+        sheet.write(0, o + 4, "author" + str(m + 1) + "_email")
+        sheet.write(0, o + 5, "author" + str(m + 1) + "_institution")
+        sheet.write(0, o + 6, "author" + str(m + 1) + "_is_corporate")
+
+        o += 7
+    return
+
+
 def main():
-    # book = xlwt.Workbook()
-    # sheet = book.add_sheet('Sheet 1')
-    # book.save("Sample.xls")
+    book = xlwt.Workbook()
+    sheet = book.add_sheet('Sheet 1')
+
+    # ---- Add function for initial sheet setup
+    createInitialHeaders(sheet)
 
     driver = webdriver.Chrome("chromedriver.exe")
     Entry_Data = ExcelEntry()
-
     driver.get("https://smallsat.org")
     linksArray = ["https://www.smallsat.org/technical-program/tech-sessions", "https://www.smallsat.org/technical-program/workshop", "https://www.smallsat.org/technical-program/keynote"]
     driver.get(linksArray[0]) # Got to Technical Sessions (After one link works this will need to loop for the others)
@@ -137,7 +258,7 @@ def main():
         sessionData = sessionTitleText.split()                  # break up session title
         # TODO---- Logic of determining "day" here *I'm working on it
 
-        convertToRomanNum(sessionData, Entry_Data, sessionTitleText)
+        convertToRomanNum(sessionData, Entry_Data, sessionTitleText, driver)
         print Entry_Data.session
         sessionsList[i].click()                                 # opens drop down list per session
 
@@ -148,7 +269,8 @@ def main():
 
         j = 2                                                   # ignore first two elements, should be 2
         while j in xrange(len(eventsList) - 1):                 # cycle through events
-
+            del Entry_Data.complete_Entry[:]
+            del Entry_Data.totalAuthorMeta[:]
             eventData = eventsList[j].text                      # data on separate lines
             splitData = eventData.splitlines()                  # lines divided into list elements
 
@@ -164,7 +286,10 @@ def main():
                 Entry_Data.title = splitData[1]
 
                 authorsAff = splitData[2].split("; ")           # puts authors with their affiliation
-                separateAuthors(authorsAff)
+                separateAuthors(authorsAff, Entry_Data)
+                print "Execl entry: " + str(Entry_Data.excelRowCount)
+                generateRowEntry(Entry_Data)
+                write_to_File(sheet, Entry_Data)
 
                         # TODO---- Entry should be written out to spreadsheet --- #
 
@@ -175,17 +300,27 @@ def main():
                 else: # do the stuff
                     print splitData[0]                          # Event Title
                     Entry_Data.title = splitData[0]
-                    print splitData[1]                          # Authors/Presenters
+                    print splitData[1]           # Authors/Presenters
+                    Entry_Data.startDate = "2018-08-00T12:00:00Z"
 
                     authorsAff = splitData[1].split("; ")       # puts authors with their affiliation
 
-                    separateAuthors(authorsAff)
+                    separateAuthors(authorsAff, Entry_Data)
+                    print "Execl entry: " + str(Entry_Data.excelRowCount)
+                    generateRowEntry(Entry_Data)
+                    write_to_File(sheet, Entry_Data)
                     # TODO---- Entry should be written out to spreadsheet --- #
 
             j += 1
         i += 1
         alternateKey = 0
 
+    createAuthorHeaders(sheet, Entry_Data.authorCounter)
+    book.save("SS2018-Metadata.xls")
+    # testbook = xlrd.open_workbook("SS2018-Metadata.xls")
+    # testsheet = testbook.sheet_by_index(0)
+    # print testsheet.cell(0, 20).value
+    # print testsheet.cell(0, 21).value
 
     time.sleep(5)
     driver.close()
--- SatMining/sandBox.py
+++ SatMining/sandBox.py
@@ -7,16 +7,32 @@ import xlwt # write
 
 import string
 
+class car():
+    num = 2
+
+def testIt(car):
+    car.num = 5
+    return
+
+def main():
+    truck = car()
+    print truck.num
+    testIt(truck)
+    print truck.num
+    return
+
+if __name__ == '__main__':
+    main()
+
 #
 # uniText = u''.join(testing).encode('utf-8')
 # testing2 = testing.decode('ascii', 'replace').replace(u'\ufffd', 'Yes')
 # print testing2
-test = "2:15"
-print test[0:2]
-print test[0]
-print test[-2]
-multiRow = '''Hi
-There'''
+test = [11, 12, 13]
+test2 = [1, 2, 3]
+test = test + test2
+print test
+
 # test = multiRow.splitlines()
 # print test[0]
 #
@@ -59,75 +75,95 @@ There'''
 # print testArray
 # print evensList
 
-driver = webdriver.Chrome("chromedriver.exe")
-# # #driver = webdriver.Firefox("")
-# # #driver = webdriver.Opera("")
-# #
-driver.get("http://smallsat.org/")
-linksArray = ["https://www.smallsat.org/technical-program/tech-sessions",
-              "https://www.smallsat.org/technical-program/workshop",
-              "https://www.smallsat.org/technical-program/keynote"]
-driver.get(linksArray[0])  # Got to Technical Sessions (After one link works this will need to loop for the others)
-time.sleep(5)
-sessionsList = driver.find_elements_by_css_selector("div[class^='demo']")
-sessionsList[0].click()  # opens drop down list per session
-time.sleep(5)
-# css = By.CSS_SELECTOR("#main-info div.main-info>h1")
-# element = driver.find_element(By.CSS_SELECTOR, "#main-info")
-# element2 = element.find_element(By.PARTIAL_LINK_TEXT, "August")
-# element2 = element.find_element_by_partial_link_text("h2['Monday']")
-element2 = driver.find_elements_by_css_selector("#main-info h2")
-print len(element2)
-correctElements = []
-for i in xrange(len(element2)):
-    print element2[i].text
-
-for i in xrange(len(element2)):
-    splitter = element2[i].text.split()
-    if splitter[0] == "Session" or splitter[0] == "Poster" or splitter[0] == "Swifty" or splitter[0] == "Posters":
-        print splitter[0]
-    else:
-        correctElements.append(element2[i])
-print len(correctElements)
-print correctElements[0].text
-print correctElements[1].text
-# if driver.find_elements_by_name("btnk").__len__() != 0: # .isEmpty() or .size() != 0
-#     print "Element found!"
-# else:
-#     print "Element does not exist!"
-# testing = "Hi! Hello it worked! "
-# testing = testing[:-4]
+# class AuthorStruct(object):
+#     fname = "NULL"
+#     mname = "NULL"
+#     lname = "NULL"
+#     aff = "NULL"
+#     corporate = "FALSE"
+# testing = AuthorStruct()
+# testing.corporate = "try this"
+# print testing.corporate
+# getHeaders = xlrd.open_workbook("SmallSat_2017_Metadata.xls")
+# pullSheet = getHeaders.sheet_by_index(0)
+# testhead = pullSheet.row(0)
+# print testhead[0].value
+# book = xlwt.Workbook()
+# sheet = book.add_sheet('Sheet 1')
+# row = sheet.row(0)
+# row.write(0, tuple["a", "b"])
+# book.save("Sample.xls")
 #
-# search = driver.find_element_by_name("q")
-# search.send_keys("Utah")
-# search.send_keys(Keys.ENTER)
+#
+# driver = webdriver.Chrome("chromedriver.exe")
+# # # #driver = webdriver.Firefox("")
+# # # #driver = webdriver.Opera("")
+# # #
+# driver.get("http://smallsat.org/")
+# linksArray = ["https://www.smallsat.org/technical-program/tech-sessions",
+#               "https://www.smallsat.org/technical-program/workshop",
+#               "https://www.smallsat.org/technical-program/keynote"]
+# driver.get(linksArray[0])  # Got to Technical Sessions (After one link works this will need to loop for the others)
 # time.sleep(5)
-# nextPage = driver.find_elements_by_css_selector("a[class^='pn']")
-# print nextPage.__len__()
-# if nextPage.__len__() != 0:
-#     print "Page Completed"
-#     nextPage[0].click()
-# else:
-#     print "Last Page"
+# sessionsList = driver.find_elements_by_css_selector("div[class^='demo']")
+# sessionsList[0].click()  # opens drop down list per session
+# time.sleep(5)
+# # css = By.CSS_SELECTOR("#main-info div.main-info>h1")
+# # element = driver.find_element(By.CSS_SELECTOR, "#main-info")
+# # element2 = element.find_element(By.PARTIAL_LINK_TEXT, "August")
+# # element2 = element.find_element_by_partial_link_text("h2['Monday']")
+# element2 = driver.find_elements_by_css_selector("#main-info h2")
+# print len(element2)
+# correctElements = []
+# for i in xrange(len(element2)):
+#     print element2[i].text
 #
-# f = open("testingCreate.txt", "w") # test "w+" - may be append on the the end, otherwise use "a" or "a+"
-# f.write(testing + "\n")
-# f.write("Hello here")
-# # f.write("") # Erases data in file when "w" is enabled
-# f.close()
-
-
-# driver.get("https://www.goodreads.com")
-# driver.find_element_by_name("user[email]").send_keys("digistudent1@gmail.com")
-# driver.find_element_by_name("user[password]").send_keys("*Bigblue17")
-# driver.find_element_by_id("remember_me").click()
+# for i in xrange(len(element2)):
+#     splitter = element2[i].text.split()
+#     if splitter[0] == "Session" or splitter[0] == "Poster" or splitter[0] == "Swifty" or splitter[0] == "Posters":
+#         print splitter[0]
+#     else:
+#         correctElements.append(element2[i])
+# print len(correctElements)
+# print correctElements[0].text
+# print correctElements[1].text
+# # if driver.find_elements_by_name("btnk").__len__() != 0: # .isEmpty() or .size() != 0
+# #     print "Element found!"
+# # else:
+# #     print "Element does not exist!"
+# # testing = "Hi! Hello it worked! "
+# # testing = testing[:-4]
+# #
+# # search = driver.find_element_by_name("q")
+# # search.send_keys("Utah")
+# # search.send_keys(Keys.ENTER)
+# # time.sleep(5)
+# # nextPage = driver.find_elements_by_css_selector("a[class^='pn']")
+# # print nextPage.__len__()
+# # if nextPage.__len__() != 0:
+# #     print "Page Completed"
+# #     nextPage[0].click()
+# # else:
+# #     print "Last Page"
+# #
+# # f = open("testingCreate.txt", "w") # test "w+" - may be append on the the end, otherwise use "a" or "a+"
+# # f.write(testing + "\n")
+# # f.write("Hello here")
+# # # f.write("") # Erases data in file when "w" is enabled
+# # f.close()
 #
-# driver.find_element_by_name("user[password]").send_keys(Keys.ENTER)
 #
-# # driver.implicitly_wait(10)
-# driver.get("https://www.goodreads.com/book/show/4671.The_Great_Gatsby?from_search=true")
-# time.sleep(10)
-# driver.close()
-#
-time.sleep(5)
-driver.quit()
+# # driver.get("https://www.goodreads.com")
+# # driver.find_element_by_name("user[email]").send_keys("digistudent1@gmail.com")
+# # driver.find_element_by_name("user[password]").send_keys("*Bigblue17")
+# # driver.find_element_by_id("remember_me").click()
+# #
+# # driver.find_element_by_name("user[password]").send_keys(Keys.ENTER)
+# #
+# # # driver.implicitly_wait(10)
+# # driver.get("https://www.goodreads.com/book/show/4671.The_Great_Gatsby?from_search=true")
+# # time.sleep(10)
+# # driver.close()
+# #
+# time.sleep(5)
+# driver.quit()
